82d636cd58f9991d5d6ef921be149670ab4decce
kevd82 <80932709+kevd82@users.noreply.github.com>
2021-11-29 16:01:01 -0800

    Add files via upload

---
+++ 01-java-fundamentals/01-Java First Program/01-Java First Program/Me.java
@@ -0,0 +1,10 @@
+java Me
+
+public class Me {
+    public static void main(String[] args) {
+        System.out.println("My name is Kevin Deming");
+        System.out.println("I am 54 years old");
+        System.out.println("My hometown is Fort Wayne, Indiana");
+
+    }
+}
