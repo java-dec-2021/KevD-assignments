ce6d36b7baaae343ccdcff0457b64a1fa5705cbc
kevd82 <80932709+kevd82@users.noreply.github.com>
2021-12-07 14:53:00 -0800

    Add files via upload

--- 01-java-fundamentals/05-basics/05-basics/Basics.java
+++ 01-java-fundamentals/05-basics/05-basics/Basics.java
@@ -1,16 +1,27 @@
 
 import java.util.ArrayList;
+import java.util.Arrays;
+
+import javax.swing.event.SwingPropertyChangeSupport;
 public class Basics {
 
     public static void main(String[] args) {
         int[] numbers = {2,22,222,3,8};
+        int y = 9;
+        int[] numbersTwo = {2,22,-2,222,-22,3,9,-11};
 
-        one255();
-        oneOdd255();
-        returnSum255();
-        arrayOdd();
-        iterateArr(numbers);
-        findMax(numbers);
+        //one255();
+        //oneOdd255();
+        //returnSum255();
+        //arrayOdd();
+        //iterateArr(numbers);
+        //findMax(numbers);
+        //getAverage(numbers);
+        //greaterThanY(numbers, y);
+        //squareValues(numbers);
+        //eliminateNegs(numbersTwo);
+        //maxMinAverage(numbers);
+        //shiftValues(numbers);
 
     }
 
@@ -38,17 +49,17 @@ public class Basics {
         return sum;
     }
 
-    public static void iterateArr(int[] numbers) {
-        for(int i = 0; i < numbers.length; i++) {
-            System.out.println(numbers[i]);
+    public static void iterateArr(int[] arr) {
+        for(int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
         }
     }
 
-    public static void findMax(int[] numbers) {
-        int max = numbers[0];
-        for(int i = 1; i < numbers.length; i++) {
-            if (numbers[i] > max) {
-                max = numbers[i];
+    public static void findMax(int[] arr) {
+        int max = arr[0];
+        for(int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
             }
         }
         System.out.println(max);
@@ -60,6 +71,83 @@ public class Basics {
             y.add(i);
         }
         System.out.println(y);
+    }
+
+    public static void getAverage(int[] arr) {
+        int sum = 0;
+        for(int i = 0; i < arr.length; i++) {
+            sum  = arr[i] + sum;
+        }
+            int average = sum/arr.length;
+            //System.out.println(sum);
+            System.out.println(average);
+        }
+
+    public static void greaterThanY(int[] arr, int y) {
+        int yPlus = 0;
+        for(int i = 0; i < arr.length; i++) {
+            if(arr[i] > y) {
+                yPlus ++;
+            }
+        }
+        System.out.println(yPlus);
+    }
+    
+    public static void squareValues(int[] arr) {
+        for(int i = 0; i < arr.length; i++) {
+            arr[i] = arr[i] * arr[i];
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void eliminateNegs(int[] arr) {
+        for(int i = 0; i < arr.length; i++) {
+            if(arr[i] < 0) {
+                arr[i] = 0;
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void maxMinAverage(int[] arr) {
+        int[] MMA = new int[3];
+
+        int max = arr[0];
+        for(int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        MMA[0] = max;
+        }
+
+        int min = arr[0];
+        for(int i = 1; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+        MMA[1] = min;
+    
+
+        int sum = 0;
+        for(int i = 0; i < arr.length; i++) {
+            sum  = arr[i] + sum;
+        }
+        int average = sum/arr.length;
+        MMA[2] = average;
+        System.out.println(Arrays.toString(MMA));
+    
+    }
+
+    public static void shiftValues(int[] arr) {
+        for(int i = 0; i < arr.length; i++) {
+            if (i == arr.length - 1) {
+                arr[i] = 0;
+                break;
+            }
+            arr[i] = arr[i+1];
+        }
+        System.out.println(Arrays.toString(arr));
     }
 
 }
